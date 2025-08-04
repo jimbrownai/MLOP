23b620f73063c8258739fc3c2df9f0dac3989e56
jimbrownai <jimbrowncn234@gmail.com>
2025-08-04 09:09:27 -0400

    add plot functions

---
+++ p3local/utils.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt 
+
+def plot_history(history):
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    plt.figure(figsize=(12, 5))
+    plt.subplot(1, 2, 1)
+    plt.plot(acc, label='Train Acc')
+    plt.plot(val_acc, label='Val Acc')
+    plt.legend()
+    plt.title("Accuracy")
+
+    plt.subplot(1, 2, 2)
+    plt.plot(loss, label='Train Loss')
+    plt.plot(val_loss, label='Val Loss')
+    plt.legend()
+    plt.title("Loss")
+    plt.show()
