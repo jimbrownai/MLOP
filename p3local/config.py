21b165d8a6ed4ee10aa585606c7499882fbd81c0
jimbrownai <jimbrowncn234@gmail.com>
2025-08-04 09:05:17 -0400

    added configuration file

---
+++ p3local/config.py
@@ -0,0 +1,8 @@
+IMG_SIZE = (224, 224)
+BATCH_SIZE = 16
+EPOCHS = 15
+VALIDATION_SPLIT = 0.2
+SEED = 42
+MODEL_SAVE_PATH = "saved_models/place_classifier_model.h5"
+CHECKPOINT_PATH = "saved_models/best_model.keras"
+LOG_DIR = "logs"
